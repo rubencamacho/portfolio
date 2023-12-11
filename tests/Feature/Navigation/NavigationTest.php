84f0405f7608a7251ab4ec42f41a26d35dd13fed
Ruben Camacho <rubencamachorguez@gmail.com>
2023-12-11 18:52:43 +0000

    Añadimos el menú de navegación

---
+++ tests/Feature/Navigation/NavigationTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature\Navigation;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class NavigationTest extends TestCase
+{
+    /** @test */
+    public function navigation_component_can_be_rendered()
+    {
+        $this->get(uri:'/')->assertStatus(status:200)->assertSeeLivewire('navigation.navigation');
+        // $this->get('/')->assertStatus(200)->assertSeeLivewire('navigation.navigation');
+
+    }
+    
+}
