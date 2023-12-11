9deb221e264cfd4c5498c16a8395610b569dacfc
Ruben Camacho <rubencamachorguez@gmail.com>
2023-12-11 20:46:07 +0000

    Creamos un componente para mostrar el menú en la versión web y mv

--- resources/views/livewire/navigation/navigation.blade.php
+++ resources/views/livewire/navigation/navigation.blade.php
@@ -20,9 +20,10 @@
                         </div>
                     </div>
                     <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8 text-lg">
-                        @foreach ($items as $item)
+                        <x-navigation.links class="text-gray-200 hover:text-red-300" :items="$items" />
+                        {{-- @foreach ($items as $item)
                             <a href="{{ $item->link }}" class="font-medium text-gray-200 hover:text-red-300">{{ $item->label }}</a>
-                        @endforeach
+                        @endforeach --}}
                         
                     </div>
                     <!-- Aqui botones edit y add -->
@@ -45,9 +46,10 @@
                         </div>
                     </div>
                     <div class="px-2 pt-2 pb-3 space-y-1">
-                        @foreach ($items as $item)
+                        <x-navigation.links class="block px-3 py-2 rounded-md text-base text-gray-700 hover:text-gray-900 hover:bg-gray-50" :items="$items" />
+                        {{-- @foreach ($items as $item)
                             <a href="{{ $item->link }}" class="font-medium block px-3 py-2 rounded-md text-base text-gray-700 hover:text-gray-900 hover:bg-gray-50" > {{ $item->label }}</a>
-                        @endforeach
+                        @endforeach --}}
                         <!-- Aqui botones edit y add -->
                     </div>
                 </div>
