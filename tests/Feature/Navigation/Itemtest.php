213cf41cb1ad0a1542b0a11bcedf79939a2d0b52
Ruben Camacho <rubencamachorguez@gmail.com>
2023-12-14 16:59:48 +0000

    Hacemos las pruebas para la creación de un item para que cumpla con las reglas establecidas

--- tests/Feature/Navigation/Itemtest.php
+++ tests/Feature/Navigation/Itemtest.php
@@ -32,4 +32,52 @@ class Itemtest extends TestCase
 
         $this->assertDatabaseHas('navitems', ['label' => 'My label', 'link' => 'My link']);
     }
+
+    /** @test */
+    public function label_is_required()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)->test(Item::class)
+            ->set('item.label', '')
+            ->set('item.link', '#myLink')
+            ->call('save')
+            ->assertHasErrors(['item.label' => 'required']);
+    }
+
+    /** @test */
+    public function label_is_max_20_characters()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)->test(Item::class)
+            ->set('item.label', 'My label is more than 20 characters')
+            ->set('item.link', '#myLink')
+            ->call('save')
+            ->assertHasErrors(['item.label' => 'max']);
+    }   
+
+    /** @test */
+    public function link_is_required()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)->test(Item::class)
+            ->set('item.label', 'My link')
+            ->set('item.link', '')
+            ->call('save')
+            ->assertHasErrors(['item.link' => 'required']);
+    }   
+
+    /** @test */
+    public function link_is_max_40_characters()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)->test(Item::class)
+            ->set('item.label', 'My link')
+            ->set('item.link', 'My link is more than 40 characters and this is a very long link')
+            ->call('save')
+            ->assertHasErrors(['item.link' => 'max']);
+    }   
 }
