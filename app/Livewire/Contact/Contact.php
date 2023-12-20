a736dd51dc5185854ad1da0fd58e6d331bb49e50
Ruben Camacho <rubencamachorguez@gmail.com>
2023-12-20 11:58:33 +0000

    Pasamos el valor del email a la vista de forma dinámica y creamos el test.

--- app/Livewire/Contact/Contact.php
+++ app/Livewire/Contact/Contact.php
@@ -3,9 +3,17 @@
 namespace App\Livewire\Contact;
 
 use Livewire\Component;
+use App\Models\PersonalInformation;
 
 class Contact extends Component
 {
+    public PersonalInformation $contact;
+
+    public function mount()
+    {
+        $this->contact = PersonalInformation::select('email')->first() ?? new PersonalInformation();
+    }
+
     public function render()
     {
         return view('livewire.contact.contact');
