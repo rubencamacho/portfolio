57708634ea380fd117d831ff605248f7b32f5da0
Ruben Camacho <rubencamachorguez@gmail.com>
2023-12-20 13:00:15 +0000

    Modificación el email de contacto y test con las reglas de validación

--- app/Livewire/Contact/Contact.php
+++ app/Livewire/Contact/Contact.php
@@ -2,16 +2,35 @@
 
 namespace App\Livewire\Contact;
 
+use App\Livewire\Traits\Notefication;
 use Livewire\Component;
 use App\Models\PersonalInformation;
+use App\Livewire\Traits\Slideover;
 
 class Contact extends Component
 {
+    use Slideover, Notefication;
+
     public PersonalInformation $contact;
+
+    protected $rules = [
+        'contact.email' => 'required|email:filter',
+    ];
 
     public function mount()
     {
-        $this->contact = PersonalInformation::select('email')->first() ?? new PersonalInformation();
+        $this->contact = PersonalInformation::first() ?? new PersonalInformation();
+    }
+
+    public function edit()
+    {
+        $this->validate();
+
+        $this->contact->save();
+
+        $this->reset('openSlideover');
+
+        $this->notify(__('Contact email updated successfully.'));
     }
 
     public function render()
--- tests/Feature/Contact/ContactTest.php
+++ tests/Feature/Contact/ContactTest.php
@@ -4,13 +4,17 @@ namespace Tests\Feature\Contact;
 
 use App\Livewire\Contact\Contact;
 use App\Models\PersonalInformation;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
+use Termwind\Components\Li;
 use Tests\TestCase;
 
 class ContactTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     public function contact_component_can_be_rendered()
     {
@@ -24,5 +28,68 @@ class ContactTest extends TestCase
 
         Livewire::test(Contact::class)
             ->assertSee($info->email);
+    }
+
+    /** @test */
+    public function only_admin_can_see_contact_action()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)
+            ->test(Contact::class)
+            ->assertStatus(200)
+            ->assertSee(__('Edit'));
+    }
+
+    /** @test */
+    public function guests_cannot_see_contact_action()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+        // Livewire::test(Contact::class)
+        //     ->assertStatus(200)
+        //     ->assertDontSee('Edit');
+        
+        // $this->assertGuest();
+    }
+
+    /** @test */
+    public function admin_can_edit_contact_email()
+    {
+        $user = User::factory()->create();
+        $contact = PersonalInformation::factory()->create();
+
+        Livewire::actingAs($user)
+            ->test(Contact::class)
+            ->set('contact.email', 'ruben@gmail.com')
+            ->call('edit');
+
+        $this->assertDatabaseHas('personal_information', [
+            'id' => $contact->id,
+            'email' => 'ruben@gmail.com'
+        ]);
+    }
+
+    /** @test */
+    public function contact_email_is_required()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)
+            ->test(Contact::class)
+            ->set('contact.email', '')
+            ->call('edit')
+            ->assertHasErrors(['contact.email' => 'required']);
+    }
+
+    /** @test */
+    public function contact_email_must_be_valid()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)
+            ->test(Contact::class)
+            ->set('contact.email', 'ruben')
+            ->call('edit')
+            ->assertHasErrors(['contact.email' => 'email']);
     }
 }
