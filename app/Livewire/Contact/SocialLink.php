34ae4fa37113729932d154222d6c09f81fa133ad
Ruben Camacho <rubencamachorguez@gmail.com>
2023-12-21 10:02:49 +0000

    Creamos el componente livewire para las RRSS y testeamos que es renderizado.

---
+++ app/Livewire/Contact/SocialLink.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Contact;
+
+use Livewire\Component;
+
+class SocialLink extends Component
+{
+    public function render()
+    {
+        return view('livewire.contact.social-link');
+    }
+}
---
+++ tests/Feature/Contact/SocialLinkTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature\Contact;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SocialLinkTest extends TestCase
+{
+    /** @test */
+    public function social_link_component_can_be_rendered()
+    {
+        $this->get('/')
+            ->assertStatus(200)
+            ->assertSeeLivewire('contact.social-link');
+    }
+}
